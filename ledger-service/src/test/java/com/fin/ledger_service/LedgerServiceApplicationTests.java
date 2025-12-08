f6b42cb078351da0e8bcdcd927cf29e26b6829af
Aayush Pathak <aayushpathak77@gmail.com>
2025-12-08 20:57:14 +0545

    added service

---
+++ ledger-service/src/test/java/com/fin/ledger_service/LedgerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fin.ledger_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedgerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
