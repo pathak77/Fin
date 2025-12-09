e548252478b52f9ecaa103e3006c721274040663
Aayush Pathak <aayushpathak77@gmail.com>
2025-12-10 02:00:52 +0545

    all done with the connection with postgres

--- friend-service/src/main/java/com/fin/friend_service/FriendServiceApplication.java
+++ friend-service/src/main/java/com/fin/friend_service/FriendServiceApplication.java
@@ -3,10 +3,13 @@ package com.fin.friend_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class FriendServiceApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kathmandu"));
 		SpringApplication.run(FriendServiceApplication.class, args);
 	}
 
